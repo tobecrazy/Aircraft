ed6ae41d1d0449a88265415aad9bf0525366ef7b
Young Liu <young.liu@sap.com>
2022-10-07 08:40:27 +0800

    add icon

--- app/src/main/java/com/young/aircraft/gui/MainActivity.kt
+++ app/src/main/java/com/young/aircraft/gui/MainActivity.kt
@@ -1,26 +1,39 @@
 package com.young.aircraft.gui
 
+import android.annotation.SuppressLint
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
+import android.os.Build
 import android.os.Bundle
 import android.os.IBinder
+import android.util.Log
 import android.view.KeyEvent
+import android.view.MotionEvent
 import android.view.View
-import android.widget.TextView
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
 import com.young.aircraft.R
 import com.young.aircraft.databinding.ActivityMainBinding
 import com.young.aircraft.service.MusicService
+import kotlin.properties.Delegates
 import kotlin.system.exitProcess
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var mService: MusicService
     private var mBound: Boolean = false
     private var exitTime: Long = 0
+    private var lastX = 0
+    private var lastY = 0
+    private var maxRight by Delegates.notNull<Int>()
+    private var maxBottom by Delegates.notNull<Int>()
 
     private val connection = object : ServiceConnection {
         override fun onServiceConnected(componentName: ComponentName?, service: IBinder?) {
@@ -35,24 +48,99 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val textView = TextView(this)
-        textView.text = getString(R.string.app_name)
-        binding.container.addView(textView)
-        binding.playLl.setOnClickListener {
+        addEnemy(5)
+        binding.playControlLayout.setOnClickListener {
             if (mBound) {
                 mService.soundPlay()
             }
-            binding.playLl.visibility = View.GONE
+            binding.playControlLayout.visibility = View.GONE
         }
 
         binding.root.setOnClickListener {
             if (mBound) {
                 mService.soundPlayShot()
             }
+        }
+        binding.jetPlane.setOnTouchListener { view, event ->
+            //get original x/y
+            val eventX = event.rawX.toInt()
+            val eventY = event.rawY.toInt()
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    //Gte right/bottom of parent
+                    maxRight = binding.container.right
+                    maxBottom = binding.container.bottom
+                    //record lastX/lastY
+                    lastX = eventX
+                    lastY = eventY
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    //calculate offset
+                    val dx: Int = eventX - lastX
+                    val dy: Int = eventY - lastY
+                    //using offset set imageView
+                    var left: Int = binding.jetPlane.left + dx
+                    var top: Int = binding.jetPlane.top + dy
+                    var right: Int = binding.jetPlane.right + dx
+                    var bottom: Int = binding.jetPlane.bottom + dy
+
+                    //set left  >=0
+                    if (left < 0) {
+                        right += -left
+                        left = 0
+                    }
+                    //set top
+                    if (top < 0) {
+                        bottom += -top
+                        top = 0
+                    }
+                    //set right <=maxRight
+                    if (right > maxRight) {
+                        left -= right - maxRight
+                        right = maxRight
+                    }
+                    //set bottom <=maxBottom
+                    if (bottom > maxBottom) {
+                        top -= bottom - maxBottom
+                        bottom = maxBottom
+                    }
+                    binding.jetPlane.layout(left, top, right, bottom)
+                    lastX = eventX
+                    lastY = eventY
+                }
+                else -> {}
+            }
+            true
+
+        }
+    }
+
+    private fun addEnemy(number: Int) {
+        val enemy_back = mutableListOf<Int>()
+        enemy_back.add(R.drawable.enemy_1)
+        enemy_back.add(R.drawable.enemy_2)
+        enemy_back.add(R.drawable.enemy_3)
+        for (i in 1..number) {
+            Log.d("YoungTest", "====> getScreenWidth() ${getScreenWidth()}")
+            Log.d("YoungTest", "====> getScreenHeight() ${getScreenHeight()}")
+            val enemy = ImageView(this)
+            val params = LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            params.width = 150
+            params.height = 150
+            enemy.layoutParams = params
+
+            enemy.rotation = 180.0f
+            enemy.scaleType = ImageView.ScaleType.FIT_CENTER
+            enemy.setImageDrawable(AppCompatResources.getDrawable(this, enemy_back[i % 3]))
+            binding.container.addView(enemy)
         }
     }
 
@@ -88,9 +176,38 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        Log.d("YoungTest", "===> $event")
+        return true
+    }
+
     override fun onStop() {
         super.onStop()
         unbindService(connection)
         mBound = false
     }
+
+    fun getScreenWidth(): Int {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            Log.d("YoungTest", "Current metrics")
+            windowManager.currentWindowMetrics.bounds.width()
+        } else {
+            Log.d("YoungTest", "Resource metrics")
+            val metrics = resources.displayMetrics
+            metrics.widthPixels
+        }
+    }
+
+    fun getScreenHeight(): Int {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            Log.d("YoungTest", "Current metrics")
+            windowManager.currentWindowMetrics.bounds.height()
+        } else {
+            Log.d("YoungTest", "Resource metrics")
+            val metrics = resources.displayMetrics
+            metrics.heightPixels
+        }
+    }
+
 }
