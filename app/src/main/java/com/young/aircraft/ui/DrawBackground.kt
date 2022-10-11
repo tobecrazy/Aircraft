4e3fc042ed670be743a7bc594792c168cd80fc50
Young Liu <young.liu@sap.com>
2022-10-11 21:07:51 +0800

    rename backgroud

---
+++ app/src/main/java/com/young/aircraft/ui/DrawBackground.kt
@@ -0,0 +1,17 @@
+package com.young.aircraft.ui
+
+import android.content.Context
+import android.graphics.Canvas
+
+/**
+ * Create by Young
+ **/
+class DrawBackground(context: Context) : DrawBaseObject(context) {
+    override fun onDraw(canvas: Canvas?) {
+
+    }
+
+    override fun updateGame() {
+
+    }
+}
