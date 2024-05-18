37bfd7b62e9e705fb1b53c2cfd03593539e11696
Young Liu <young.liu@sap.com>
2024-05-18 12:56:23 +0800

    up

--- app/src/main/java/com/young/aircraft/gui/PrivacyPolicyActivity.kt
+++ app/src/main/java/com/young/aircraft/gui/PrivacyPolicyActivity.kt
@@ -6,6 +6,7 @@ import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import com.young.aircraft.BuildConfig
 import com.young.aircraft.R
 
 class PrivacyPolicyActivity : AppCompatActivity() {
@@ -15,14 +16,19 @@ class PrivacyPolicyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_privacy_policy)
-        WebView.setWebContentsDebuggingEnabled(true);
+        if (BuildConfig.DEBUG) {
+            WebView.setWebContentsDebuggingEnabled(true)
+        }
         webView = findViewById(R.id.web_view)
-        webView.settings.javaScriptEnabled = true;
+        webView.settings.allowFileAccess = true
+        webView.settings.javaScriptEnabled = true
+        webView.settings.loadsImagesAutomatically = true
+        webView.settings.layoutAlgorithm = WebSettings.LayoutAlgorithm.SINGLE_COLUMN
+        webView.settings.loadWithOverviewMode = true
         webView.webViewClient = WebViewClient()
         webView.apply {
             loadUrl("file:///android_asset/privacy policy.html")
-            settings.layoutAlgorithm = WebSettings.LayoutAlgorithm.SINGLE_COLUMN
-            settings.loadWithOverviewMode = true
+
         }
     }
 }
